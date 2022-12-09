10d59c754ff61648c8cfb3c2f97a81458955d0f9
macjayz <machenjosephjohn@gmail.com>
2022-12-09 06:54:54 -0800

    New command added

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+for (int num = 0; num <= 9; num++)
+{
+printf("%d",num);
+}
+printf("\n");
+return (0);
+}
