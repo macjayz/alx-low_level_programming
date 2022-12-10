95e96344e212893877d370aac19ca11fe41cad36
macjayz <machenjosephjohn@gmail.com>
2022-12-10 12:54:07 -0800

    New code added

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,22 +8,25 @@
 */
 int main(void)
 {
-int num1;
-int num2;
-for (num1 = 0; num1 <= 98; num1++)
+int i,j;
+for (i = 0; i < 100; i++) 
 {
-for (num2 = num1 + 1; num2 <= 99; num2++)
+for (j = 0; j < 100; j++)
 {
-putchar((num1 / 10) + '0');
-putchar((num1 % 10) + '0');
+if (i < j)
+{
+putchar((i / 10) + 48);
+putchar((i % 10) + 48);
 putchar(' ');
-putchar((num2 / 10) + '0');
-putchar((num1 % 10) + '0');
-
-if (num1 == 98 && num2 == 99)
-continue;
+putchar((j / 10) + 48);
+putchar((j % 10) + 48);
+if (i != 98 || j != 99)
+{
 putchar(',');
 putchar(' ');
+}
+
+}
 }
 }
 putchar('\n');
