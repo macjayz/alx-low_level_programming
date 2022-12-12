8accd1455e23e9c1dcaa8149305d3fe537690e6a
macjayz <machenjosephjohn@gmail.com>
2022-12-12 03:08:50 -0800

    New adjustement added

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,11 +11,10 @@ int main(void)
 int n;
 for (n = 0; n <= 9; n++)
 {
-putchar((n % 10) + '0');
+putchar(n + '0');
 if (n == 9)
 continue;
 putchar(',');
-putchar(' ');
 }
 putchar('\n');
 return (0);
