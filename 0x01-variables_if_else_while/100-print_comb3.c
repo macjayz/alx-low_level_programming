1f68e37eae8c0ca0da9924818c94e72920162e61
macjayz <machenjosephjohn@gmail.com>
2022-12-12 04:02:05 -0800

    New adjustment

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,9 +11,11 @@ for (i = 0; i <= 8; i++)
 {
 	for (j = 0; j <= 9; j++)
 	{
+	if (i == j)
+	continue;
 	putchar(i + '0');
 	putchar(j + '0');
-	if (i == 8 && j == 9)
+	if (i == 8 && j == 9) 
 	continue;
 	putchar(',');
 	}
