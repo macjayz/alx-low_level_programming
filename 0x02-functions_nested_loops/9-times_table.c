0aef494397af43195a4bff1ad80a43d21a8f2dc8
macjayz <machenjosephjohn@gmail.com>
2022-12-13 11:18:34 -0800

    New command

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+*times_table - This function print the 9 times table
+*
+*Return: Always 0 (Success)
+*/
+void times_table(void)
+{
+int row, col, product, tens, ones;
+
+for (row = 0; row <= 9; row++)
+{
+for (col = 0; col <= 9; col++)
+{
+product = row * column;
+tens = product / 10;
+ones = product % 10;
+
+if (column == 0)
+{
+_putchar('0');
+}
+else if (product < 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(tens + '0')
+_putchar(ones + '0');
+}
+}
+_putchar('\n');
+}
+}
