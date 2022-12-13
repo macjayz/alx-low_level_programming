a6caae204d1fd178c9d0d3bc515e259780c6e5f8
macjayz <machenjosephjohn@gmail.com>
2022-12-13 11:26:44 -0800

    New command

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -24,6 +24,13 @@ else if (product < 10)
 {
 _putchar(',');
 _putchar(' ');
+_putchar(' ');
+_putchar(ones + '0');
+}
+else
+{
+_putchar(',');
+_putchar(' ');
 _putchar(tens + '0');
 _putchar(ones + '0');
 }
