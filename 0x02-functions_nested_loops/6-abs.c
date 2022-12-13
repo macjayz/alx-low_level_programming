be5ec75eea61a48b7cddbbfd98011546cf0491fc
macjayz <machenjosephjohn@gmail.com>
2022-12-13 04:59:54 -0800

    New script added

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*_abs - This function computes the absolute value of an integer
+*
+*Return: Always 0 (Success)
+*
+*@ans: This variable receives a value from the main.c
+*/
+int _abs(int ans)
+{
+ans *= -1;
+return (ans);
+}
