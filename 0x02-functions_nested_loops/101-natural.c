6d949803bdef40f02ed4c0daa505bba73d489c89
macjayz <machenjosephjohn@gmail.com>
2022-12-13 14:03:33 -0800

    New code added

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i, z = 0;
+while (i < 1024)
+{
+if ((i % 3 == 0) || (i % 5 == 0))
+{
+z += 1;
+}
+i++;
+}
+printf("%d\n", z);
+return (0);
+}
