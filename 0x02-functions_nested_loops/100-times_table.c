c2bbb9ae0260291fb966c6f01272849c38cdeb3e
macjayz <machenjosephjohn@gmail.com>
2022-12-13 13:44:33 -0800

    New code added

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,9 +11,9 @@ void print_times_table(int n)
 int x, y, z;
 if (n >= 0 && n <= 14)
 {
-for (x = 0; x <= 9; x++)
+for (x = 0; x <= n; x++)
 {
-for (y = 0; y <= 9; y++)
+for (y = 0; y <= n; y++)
 {
 z = x * y;
 if (z > 99)
