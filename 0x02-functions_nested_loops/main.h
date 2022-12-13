4a9ae095df8ec08fbe7370464a8234566f4df783
macjayz <machenjosephjohn@gmail.com>
2022-12-13 03:22:43 -0800

    New commit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,7 @@
 #include <unistd.h>
 
 
-int _putchar(char n)
-{
-	return (write(1, &n, 1));
-}
+int _putchar(char n);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
