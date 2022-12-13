4aec7b958243c89df9ecc7861a013c8313bbc1e9
macjayz <machenjosephjohn@gmail.com>
2022-12-13 10:30:19 -0800

    New command added

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*jack_bauer - This print time in 24 hours
+*
+*Return: Always 0 (Success)
+*/
+void jack_bauer(void)
+{
+int i, j;
+for (i = 0; i < 24; i++)
+{
+_putchar(i + '0');
+_putchar(':');
+_putchar(j + '0');
+}
+_putchar('\n');
+}
