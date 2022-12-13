29f440a71f26afdfb7da109a9e4a7073791898af
macjayz <machenjosephjohn@gmail.com>
2022-12-13 10:48:13 -0800

    New command

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,7 +9,7 @@ void jack_bauer(void)
 int hours_tens, hours_ones, minutes_tens, minutes_ones, hours_max;
 
 hours_max = 58;
-hiours_tens = 0;
+hours_tens = 0;
 while (hours_tens < '3')
 {
 if (hours_tens == '2')
@@ -27,7 +27,7 @@ while (minutes_ones < 58)
 _putchar(hours_tens);
 _putchar(hours_ones);
 _putchar(':');
--putchar(minutes_ones);
+_putchar(minutes_ones);
 _putchar('\n');
 minutes_ones++;
 }
