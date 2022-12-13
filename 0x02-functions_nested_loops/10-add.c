d809b23e5283636baff407a93ced0da4478818fd
macjayz <machenjosephjohn@gmail.com>
2022-12-13 11:35:23 -0800

    New code added

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*add - this function sums to numbers together
+*
+*Return: Always 0 (Success)
+*
+*@num1: This is the first number
+*
+*@num2: This is the second number
+*/
+int add(int num1, int num2)
+{
+int ans;
+
+ans = num1 + num2;
+
+return (ans);
+}
