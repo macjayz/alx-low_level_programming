83ed626ce1ebfe9db4b6e2ed20835948357757b5
macjayz <machenjosephjohn@gmail.com>
2022-12-14 05:37:08 -0800

    New code added

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,7 @@
 #include "main.h"
 /**
-*print_alphabet_x10 - Entry point
-*
-*Return: Always 0 (Success)
+* main - tests function that prints if integer is positive or negative
+* Return: 0
 */
 int main(void)
 {
@@ -10,6 +9,5 @@ int i;
 i = 98;
 
 positive_or_negative(i);
-
 return (0);
 }
