0b0e1e01c300a2e882ed4e801c79910d45f8765d
macjayz <machenjosephjohn@gmail.com>
2022-12-14 00:43:53 -0800

    New code added

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*print_alphabet_x10 - Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void){
+int i;
+i = 98;
+
+positive_or_negative(i);
+
+
+return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+This directory contains codes that help us debug our file
