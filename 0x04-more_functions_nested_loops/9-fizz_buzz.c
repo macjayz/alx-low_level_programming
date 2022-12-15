8ae7c1abe8ce4f7d92a2597c1330eddebc67c2a8
macjayz <machenjosephjohn@gmail.com>
2022-12-15 14:22:18 -0800

    New code

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,35 +1,36 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
-*main - main entry
-*
-*Return: 0
-*/
+ * main - function that checks for uppercase character.
+ * Return: 0
+ */
 
 int main(void)
 {
-int i;
-for (i = 1; i <= 100; i++)
-{
-if ((i % 3) == 0)
-{
-printf("Fizz");
-}
-else if ((i % 5) == 0)
-{
-printf("Buzz");
-}
-else if ((i % 15) == 0)
-{
-printf("FizzBuzz");
-}
-else
-{
-printf("%d", i);
-}
-if (i < 100)
-printf(" ");
-}
-printf("\n");
+
+	int h = 1;
+
+
+	while (h <= 100)
+	{
+		if (h % 3 == 0 && h % 5 == 0)
+			printf("FizzBuzz ");
+		else if (h % 5 == 0)
+		{
+			if (h == 100)
+			{
+				printf("Buzz");
+				printf("\n");
+			}
+			else
+				printf("Buzz ");
+		}
+		else if (h % 3 == 0)
+			printf("Fizz ");
+		else
+			printf("%d ", h);
+	h++;
+	}
 return (0);
+
 }
