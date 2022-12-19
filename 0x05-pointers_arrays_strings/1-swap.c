42eedd7ce136c45f659f1a0e9223db9adffad920
macjayz <machenjosephjohn@gmail.com>
2022-12-19 06:14:27 -0800

    New code added

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+* swap_int - This swaps the integer values of a and b
+*
+* Return: Always 0.
+*@a: This accepts pointer a argument
+*@b: This accepts pointer b argument
+*/
+
+void swap_int(int *a, int *b)
+{
+
+*a = 42;
+*b = 98;
+}
