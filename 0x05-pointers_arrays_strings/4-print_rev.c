e1b3f87932d5ae1bf32be09882b200742ebc40f6
macjayz <machenjosephjohn@gmail.com>
2022-12-21 06:34:28 -0800

    New code added

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -13,11 +13,13 @@
 void print_rev(char *str)
 {
 int i;
-int length = strlen(str);
-for (i = length - 1; str[i] >= 0; i--)
+int length = 0;
+while (str[length] != '\0')
+length++;
+
+for (i = length - 1; i >= 0; i--)
 {
 _putchar(str[i]);
-
 }
 _putchar(10);
 }
