ce00e9ae94b2f00761da1d12b2f765b41a9bc50d
macjayz <machenjosephjohn@gmail.com>
2022-12-22 10:52:13 -0800

    New code added

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,25 +1,22 @@
-#include <stdio.h>
-/**
-*main - Entry point
-*Return: 0
-*/
+nclude <stdio.h>
+
 
 
 int main(void)
 
 {
 
-int n;
+  int n;
 
-int a[5];
+  int a[5];
 
-int *p;
+  int *p;
 
 
 
-a[2] = 1024;
+  a[2] = 1024;
 
-p = &n;
+  p = &n;
 
   /*
    * write your line of code here...
@@ -30,12 +27,12 @@ p = &n;
    * - you are not allowed to code anything else than this line of code
    */
 
-*(p + 5) = 98;
+ p[5] = 98;
 
-/* ...so that this prints 98\n */
+  /* ...so that this prints 98\n */
 
-printf("a[2] = %d\n", a[2]);
+  printf("a[2] = %d\n", a[2]);
 
-return (0);
+  return (0);
 
 }
