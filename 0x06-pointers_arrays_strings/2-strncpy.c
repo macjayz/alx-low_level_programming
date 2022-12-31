0812a57f8ccc5b2458c7aaf0cb685f7abfd4d78d
macjayz <machenjosephjohn@gmail.com>
2022-12-31 12:56:52 -0800

    New code added

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,45 +1,28 @@
 #include "main.h"
-
-
+#include <stdio.h>
+#include <string.h>
 
 /**
- * _strncpy - compares two strings
- * @dest: the first string.
- * @src: the second string.
- * @n: third integer argument.
- * Return: 0 if s1 and s2 are equals
- */
-
-
+* _strncpy - This function prints a string
+*
+*Return: 0
+*@dest: This accepts char argument
+*@src: This accepts char argument
+*@n: This accepts int argument
+*/
 
 char *_strncpy(char *dest, char *src, int n)
 {
-
 int i;
 
+for (i = 0;  i < n && src[i] != '\0'; i++)
+{
 
+*(dest + i) = *(src + i);
+}
 
-	i = 0;
+if (i < n)
+dest[i] = '\0';
 
-	while (i < n && *(src + i))
-
-	{
-
-		*(dest + i) = *(src + i);
-
-		i++;
-
-	}
-
-	while (i < n)
-
-	{
-
-		*(dest + i) = '\0';
-
-		i++;
-
-	}
-
-	return (dest);
+return (dest);
 }
