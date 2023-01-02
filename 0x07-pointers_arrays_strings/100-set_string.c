e14c0382c388d665bfeaf8d7f2249c02d0fe8b66
macjayz <machenjosephjohn@gmail.com>
2023-01-02 13:26:15 -0800

    New code added

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+*set_string - This function prints a string
+*
+*Return: 0
+*@s: This accepts char argument
+*@to: This accepts char argument argument
+*/
+
+void set_string(char **s, char *to)
+
+{
+
+	*s = to;
+
+}
