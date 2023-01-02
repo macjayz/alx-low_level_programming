9f702e092def16437b836d27251e798a2dedcb51
macjayz <machenjosephjohn@gmail.com>
2023-01-02 03:15:21 -0800

    New code added

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -13,14 +13,12 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-unsigned int  i;
+unsigned int i;
 
 for (i = 0; i < n; i++)
 {
 *(dest + i) = *(src + i);
 }
-
-dest[i] = '\0';
 
 return (dest);
 }
