274e0640318b2cd346d73c962fe00bc2c0e7d4d6
macjayz <machenjosephjohn@gmail.com>
2023-01-03 03:30:25 -0800

    New code

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -3,29 +3,19 @@
 
 
 /**
-
 * print_chessboard - Prints chessboard
-
 *
-
 * @a: Double pointer
-
 *
-
 * Return: 0
-
 */
 
 void print_chessboard(char (*a)[8])
-
 {
 
 	unsigned int i, j;
 
-
-
 	for (i = 0; i < 8; i++)
-
 	{
 
 		for (j = 0; j < 8; j++)
