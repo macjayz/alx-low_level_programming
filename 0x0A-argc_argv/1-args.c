9bc815ef2d30e46d92738bbb919e6d06dfb9cbc8
macjayz <machenjosephjohn@gmail.com>
2023-01-06 14:06:58 -0800

    New code added

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*main - Entry point
+*
+*Return: 0
+*@argc: first integer argument
+*@argv: second integer argument
+*/
+
+int main(int argc, char *argv[] __attribute__ ((unused)))
+{
+printf("%d\n", argc);
+return (0);
+}
+
