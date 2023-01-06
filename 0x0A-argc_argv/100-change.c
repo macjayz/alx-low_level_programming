f92d14e31afd439c426ae409c9e4b2420b5fe52f
macjayz <machenjosephjohn@gmail.com>
2023-01-06 15:26:09 -0800

    New code added

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -15,27 +15,18 @@
 int main(int argc, char *argv[])
 {
 	int result, amount, cent;
-
-
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-
 	result = 0;
 	amount  = atoi(argv[1]);
-
-
 	if (amount < 0)
 	{
 		printf("0\n");
 		return (0);
-
 	}
-
-
 	while (amount)
 	{
 		if (amount >= 25)
@@ -48,11 +39,9 @@ int main(int argc, char *argv[])
 			cent = 2;
 		else
 			cent = 1;
-
 		result += amount / cent;
 		amount %= cent;
 	}
-
 	printf("%d\n", result);
 	return (0);
 }
