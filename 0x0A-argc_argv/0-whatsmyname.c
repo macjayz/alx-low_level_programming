ff68740f0c2a500ed0e2c74ef17caceebcf5918b
macjayz <machenjosephjohn@gmail.com>
2023-01-06 13:26:27 -0800

    New code added

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*
+*Return: 0
+*@argc: first integer argument
+*@argv: second integer argument
+*/
+
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+
+}
+
