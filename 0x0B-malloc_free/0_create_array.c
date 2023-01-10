4b0c98669dbb66d9dd829772ae8f07a9f6c4cb8d
macjayz <machenjosephjohn@gmail.com>
2023-01-10 04:24:54 -0800

    New code added

---
+++ 0x0B-malloc_free/0_create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+*create_array - This function creates an array
+*Return: 0
+*@size: this accepts int argument of size
+*@c: This accepts char argument
+*/
+
+char *create_array(unsigned int size, char c)
+{
+if (size == 0)
+return (NULL);
+
+int i;
+char *arr = malloc(sizeof(char) * size);
+
+for (i = 0; i < size; i++)
+{
+arr[i] = c;
+}
+
+if (arr)
+return (arr);
+else
+return (NULL);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+This directory contains files on memory allocation
