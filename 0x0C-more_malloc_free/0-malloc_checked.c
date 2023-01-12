8ffecbe9a99cc02257e3b9f5e3d23ed1c6cff187
macjayz <machenjosephjohn@gmail.com>
2023-01-12 12:26:07 -0800

    New code added

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -5,25 +5,19 @@
 /**
 *malloc_checked - This function checks for malloc
 *
-*@Return: 0
+*Return: void
 *@b: This accepts int argument
 */
 
 void *malloc_checked(unsigned int b)
-
 {
 
 	void *p;
 
-
-
 	p = malloc(b);
 
 	if (p == NULL)
-
 		exit(98);
-
-
 
 	return (p);
 
