bb1e31315f4b6ccbaf262816a4d092ec69e4e6eb
macjayz <machenjosephjohn@gmail.com>
2023-01-16 11:01:31 -0800

    New code added

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,9 +11,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-
-d->name;
-d->age;
-d->owner;
-
+if (d != NULL)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
 }
