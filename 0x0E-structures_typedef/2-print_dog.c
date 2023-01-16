3f25de2e5de4d5054375e5d14c8e9fc04d6081d7
macjayz <machenjosephjohn@gmail.com>
2023-01-16 12:16:47 -0800

    New code added

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,17 +12,17 @@ if (d == NULL)
 return;
 
 if (d->name == NULL)
-printf("(nil)");
+printf("name: (nil)\n");
 else
 printf("name: %s\n", d->name);
 
 if (d->age < 0)
-printf("(nil)");
+printf("age: (nil)\n");
 else
 printf("age: %f\n", d->age);
 
 if (d->owner == NULL)
-printf("(nil)");
+printf("owner: (nil)\n");
 else
 printf("owner: %s\n", d->owner);
 
