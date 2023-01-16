d604866f231bd49989cdaca565042a78b4dffb24
macjayz <machenjosephjohn@gmail.com>
2023-01-16 13:48:00 -0800

    New code added

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,11 +16,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 
 dog_t *dog;
-if (name == NULL || age < 0 || owner == NULL)
-		return (NULL);
 
 dog = malloc(sizeof(dog_t));
-if (!dog)
+if (dog == NULL)
 {
 return (NULL);
 }
