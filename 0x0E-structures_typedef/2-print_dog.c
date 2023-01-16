eb1d2a1940b803e388c5306fa066e1ec42c96c25
macjayz <machenjosephjohn@gmail.com>
2023-01-16 11:27:05 -0800

    New code added

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+*print_dog - Dog struct function
+*@d: the struct variable to be intialized
+*/
+
+void print_dog(struct dog *d)
+{
+if (d == NULL)
+printf(" ");
+
+if (d->name == NULL)
+printf("(nil)");
+else
+printf("name: %s\n", d->name);
+
+if (d->age == 0.0)
+printf("(nil)");
+else
+printf("age: %f\n", d->age);
+
+if (d->owner == NULL)
+printf("(nil)");
+else
+printf("owner: %s\n", d->owner);
+
+}
