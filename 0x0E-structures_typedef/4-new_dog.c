8abc5a2414b6fabf4054f59e213f92ff5c338540
macjayz <machenjosephjohn@gmail.com>
2023-01-16 16:39:44 -0800

    New code added

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,45 +1,82 @@
-#include <stdio.h>
-#include <string.h>
 #include "dog.h"
+#include <stdlib.h>
 
-/**
-*new_dog - Dog struct function
-*@name: the struct variable to be intialized
-*@age: age argument
-*@owner: owner argument
-*Return: new_dog
-*/
-
+int _strlen(char *str);
+char *_strcopy(char *dest, char *src);
 dog_t *new_dog(char *name, float age, char *owner);
 
+/**
+ * _strlen - Finds the length of a string.
+ * @str: The string to be measured.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *str)
+{
+	int len = 0;
+
+	while (*str++)
+		len++;
+	return (len);
+}
+
+/**
+ * _strcopy - Copies a string pointed to by src, including the
+ *		terminating null byte, to a buffer pointed to by dest.
+ * @dest: The buffer storing the string copy.
+ * @src: The source string.
+ *
+ * Return: The pointer to dest.
+ */
+char *_strcopy(char *dest, char *src)
+{
+	int index = 0;
+
+	for (index = 0; src[index]; index++)
+		dest[index] = src[index];
+
+	dest[index] = '\0';
+
+	return (dest);
+}
+
+/**
+ * new_dog - Creates a new dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ *
+ * Return: The new struct dog.
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	dog_t *doggo;
 
-dog_t *dog;
+	if (name == NULL || age < 0 || owner == NULL)
+		return (NULL);
 
-dog = malloc(sizeof(dog_t));
-if (dog == NULL)
-{
-return (NULL);
+	doggo = malloc(sizeof(dog_t));
+	if (doggo == NULL)
+		return (NULL);
+	doggo->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if (doggo->name == NULL)
+	{
+		free(doggo);
+		return (NULL);
+	}
+
+	doggo->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if (doggo->owner == NULL)
+	{
+		free(doggo->name);
+		free(doggo);
+		return (NULL);
+	}
+
+	doggo->name = _strcopy(doggo->name, name);
+	doggo->age = age;
+	doggo->owner = _strcopy(doggo->owner, owner);
+
+	return (doggo);
 }
 
-dog->name = malloc(sizeof(char) * (strlen(name) + 1));
-if (!dog->name)
-{
-free(dog);
-return (NULL);
-}
-
-dog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
-if (!dog->owner)
-{
-free(dog);
-return (NULL);
-}
-
-dog->name = strcpy(dog->name, name);
-dog->age = age;
-dog->owner = strcpy(dog->owner, owner);
-
-return (dog);
-}
---
+++ 0x0E-structures_typedef/unresolved.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+#include "dog.h"
+
+/**
+*new_dog - Dog struct function
+*@name: the struct variable to be intialized
+*@age: age argument
+*@owner: owner argument
+*Return: new_dog
+*/
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
+dog_t *dog;
+
+dog = malloc(sizeof(dog_t));
+if (dog == NULL)
+{
+return (NULL);
+}
+
+dog->name = malloc(sizeof(char) * (strlen(name) + 1));
+if (!dog->name)
+{
+free(dog);
+return (NULL);
+}
+
+dog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
+if (!dog->owner)
+{
+free(dog);
+return (NULL);
+}
+
+dog->name = strcpy(dog->name, name);
+dog->age = age;
+dog->owner = strcpy(dog->owner, owner);
+
+return (dog);
+}
