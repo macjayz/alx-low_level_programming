2e2b9719ff4a5f4512f261d46eee8ad196ab3b9d
macjayz <machenjosephjohn@gmail.com>
2023-01-18 04:10:27 -0800

    New code

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,10 +10,10 @@
 */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+unsigned int i;
 if (!array || !action)
 return;
 
-unsigned int i;
 for (i = 0; i < size; i++)
 {
 action(array[i]);
