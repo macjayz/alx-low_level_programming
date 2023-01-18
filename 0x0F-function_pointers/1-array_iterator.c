dc6087ed2f680ed99fd9515a56b5e630c9f5741f
macjayz <machenjosephjohn@gmail.com>
2023-01-18 03:53:36 -0800

    New code added

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+*array_iterator - Entry point to print array
+*@array: receives array argument
+*@size: receives unsigned int size argument
+*@action: a pointer to a function passed as an argument
+*Return: void
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+int i;
+for (i = 0; i < size; i++)
+{
+(*action)(array[i]);
+}
+}
