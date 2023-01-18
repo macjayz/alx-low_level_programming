7ba796b67245f47408ece9a7c63da5ce1a28b559
macjayz <machenjosephjohn@gmail.com>
2023-01-18 04:05:57 -0800

    New code added

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,21 +1,21 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include "function_pointers.h"
 
 /**
-*array_iterator - Entry point to print array
-*@array: receives array argument
-*@size: receives unsigned int size argument
-*@action: a pointer to a function passed as an argument
-*Return: void
-*/
+ * array_iterator - executes a function given as a parameter
+ * on each element of an array
+ * @array: array to iterate over
+ * @size: size of the array
+ * @action: pointer to function used
+ */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-if (!array || !size || !action)
-return;
+	unsigned int i;
 
-unsigned int i;
-for (i = 0; i < size; i++)
-{
-action(array[i]);
+	if (!array || !action)
+		return;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
 }
-}
+
---
+++ 0x0F-function_pointers/unresolved.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+*array_iterator - Entry point to print array
+*@array: receives array argument
+*@size: receives unsigned int size argument
+*@action: a pointer to a function passed as an argument
+*Return: void
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+if (!array || !size || !action)
+return;
+
+unsigned int i;
+for (i = 0; i < size; i++)
+{
+action(array[i]);
+}
+}
