13f89e76e579a5810c9a1073af4fb212ec495965
macjayz <machenjosephjohn@gmail.com>
2023-01-19 02:48:47 -0800

    New code added

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,16 +2,14 @@
 #include "function_pointers.h"
 
 /**
-*print_name - Entry point
-*@name: receives name argument
-*@f: a pointer to a function passed as an argument
-*Return: void
-*/
-
+ * print_name - prints a name
+ * @name: name to print
+ * @f: pointer to the printing function
+ */
 void print_name(char *name, void (*f)(char *))
 {
-if (!name || !f)
-return;
-
-f(name);
+	if (!name || !f)
+		return;
+	f(name);
 }
+
