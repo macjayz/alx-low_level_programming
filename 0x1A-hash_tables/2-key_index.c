9b815324b424ffba7ececa191da18cf9d80da9cb
macjayz <macjayzz1@gmail.com>
2023-04-07 14:24:48 -0700

    New commit

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - main function
+ * @key: key
+ * @size: size
+ *
+ * Return: The index of the key.
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
