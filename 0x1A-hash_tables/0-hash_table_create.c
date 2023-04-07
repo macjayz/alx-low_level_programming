b418ab0fedbf8bae1615563d22f372e7f61d7009
macjayz <macjayzz1@gmail.com>
2023-04-07 13:43:30 -0700

    New commit

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,21 +9,17 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *hash_table_created;
-
-    hash_table_created = calloc(1, sizeof(hash_table_t));
-    if (!hash_table_created)
-        return NULL;
-
-    hash_table_created->array = calloc(size, sizeof(hash_node_t *));
-    if (!hash_table_created->array)
-    {
-        free(hash_table_created);
-        return NULL;
-    }
-
-    hash_table_created->size = size;
-
-    return hash_table_created;
+hash_table_t *hash_table_created;
+hash_table_created = calloc(1, sizeof(hash_table_t));
+if (!hash_table_created)
+return (NULL);
+hash_table_created->array = calloc(size, sizeof(hash_node_t *));
+if (!hash_table_created->array)
+{
+free(hash_table_created);
+return (NULL);
+}
+hash_table_created->size = size;
+return (hash_table_created);
 }
 
