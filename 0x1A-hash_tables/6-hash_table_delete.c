3f97af1b69fe8ae938763414458e7d3dfbca66a8
macjayz <macjayzz1@gmail.com>
2023-04-08 14:59:43 -0700

    New commit

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+* hash_table_delete - Deletes a hash table.
+* @ht: A pointer to a hash table.
+*/
+
+void hash_table_delete(hash_table_t *ht)
+{
+	if (ht == NULL)
+		return;
+
+	for (unsigned int i = 0; i < ht->size; i++)
+	{
+		hash_node_t *node = ht->array[i];
+
+		while (node != NULL)
+		{
+			hash_node_t *tmp = node;
+
+			node = node->next;
+			free(tmp->key);
+			free(tmp->value);
+			free(tmp);
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}
+
