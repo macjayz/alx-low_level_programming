b3f0694c278af49d842bf7e582594864dba24e41
macjayz <macjayzz1@gmail.com>
2023-04-08 15:04:55 -0700

    New commit

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -9,22 +9,24 @@ void hash_table_delete(hash_table_t *ht)
 {
 if (ht == NULL)
 return;
-for (unsigned int i = 0; i < ht->size; i++)
+
+for (size_t i = 0; i < ht->size; i++)
 {
 hash_node_t *node = ht->array[i];
 
 while (node != NULL)
 {
-hash_node_t *tmp = node;
-
-node = node->next;
-free(tmp->key);
-free(tmp->value);
-free(tmp);
+hash_node_t *next = node->next;
+free(node->key);
+free(node->value);
+free(node);
+node = next;
 }
+ht->array[i] = NULL;
 }
-
 free(ht->array);
+ht->array = NULL;
+ht->size = 0;
 free(ht);
 }
 
