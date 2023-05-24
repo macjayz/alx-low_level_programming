1dd31f8eb206682e85fd9e420a676df839a61602
macjayz <macjayzz1@gmail.com>
2023-05-24 14:22:49 -0700

    New commit

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,18 +1,32 @@
 #!/usr/bin/python3
+""" Fuction that returns the parameter of an island"""
+
+def count_water_neighbors(grid, row, col):
+    """ this returns the number of water neighbors a cell has in a grid."""
+
+    count = 0
+    height = len(grid)
+    width = len(grid[0])
+
+    if row > 0 and not grid[row - 1][col]:
+        count += 1
+    if col > 0 and not grid[row][col - 1]:
+        count += 1
+    if col < width - 1 and not grid[row][col + 1]:
+        count += 1
+    if row < height - 1 and not grid[row + 1][col]:
+        count += 1
+
+    return count
+
 
 def island_perimeter(grid):
-    """Func that returns the perimeter of the island described in grid.
-    """
-    perimeter = 0
+    """Returns the perimeter of the island in grid."""
 
-    for i in range(len(grid)):
-        for j in range(len(grid[0])):
-            if grid[i][j] == 1:
-                perimeter += 4
-                if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 2
-                if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 2
+    perimeter = 0
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            if grid[row][col]:
+                perimeter += 4 - count_water_neighbors(grid, row, col)
 
     return perimeter
-
